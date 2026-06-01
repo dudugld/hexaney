78e223de15c9fea5edc51e9e0419e073f649ddd3
dudugld <neto.sousa@escolar.ifrn.edu.br>
2026-06-01 11:18:19 +0000

    aula dia 01/06

--- main.js
+++ main.js
@@ -20,16 +20,52 @@
 // pesoP.innerHTML = "Peso: " + peso + " kg"
 // imcP.innerHTML = "IMC: " + imc
 
-const nome = prompt("Digite o seu nome: ")
-const tamanhoNome = nome.length
-const segundaLetra = nome.charAt(1)
-const primeiraLetra = nome.charAt(0)
-const nomeMAIUSCULO = nome.toUpperCase()
-const nomeminusculo = nome.toLowerCase()
+//const nome = prompt("Digite o seu nome: ")
+//const tamanhoNome = nome.length
+//const segundaLetra = nome.charAt(1)
+//const primeiraLetra = nome.charAt(0)
+//const nomeMAIUSCULO = nome.toUpperCase()
+//const nomeminusculo = nome.toLowerCase()
 
-document.body.innerHTML += `O seu nome é: ${nome} <br>`
-document.body.innerHTML += `O seu nome tem ${tamanhoNome} letras <br>`
-document.body.innerHTML += `A segunda letra do seu nome é: ${segundaLetra} <br>`
-document.body.innerHTML += `A primeira letra do seu nome é: ${primeiraLetra} <br>`
-document.body.innerHTML += `Seu nome em MAIUSCULO: ${nomeMAIUSCULO} <br>`
-document.body.innerHTML += `Seu nome em minusculo: ${nomeminusculo} <br>`
+//document.body.innerHTML += `O seu nome é: ${nome} <br>`
+//document.body.innerHTML += `O seu nome tem ${tamanhoNome} letras <br>`
+//document.body.innerHTML += `A segunda letra do seu nome é: ${segundaLetra} <br>`
+//document.body.innerHTML += `A primeira letra do seu nome é: ${primeiraLetra} <br>`
+//document.body.innerHTML += `Seu nome em MAIUSCULO: ${nomeMAIUSCULO} <br>`
+//document.body.innerHTML += `Seu nome em minusculo: ${nomeminusculo} <br>`
+
+const respostaPrompt = prompt('Digite um número: ');
+
+const numero = Number(respostaPrompt);
+
+const numeroTitulo = document.getElementById("numero-titulo");
+numeroTitulo.innerHTML = respostaPrompt;
+
+const ehNaN = Number.isNaN(numero) || respostaPrompt.trim() === "";
+
+if (ehNaN) {
+    document.getElementById('nan').innerHTML = "Sim";
+    document.getElementById('raiz').innerHTML = "Não é um número válido";
+    document.getElementById('inteiro').innerHTML = "Não é um número válido";
+    document.getElementById('cima').innerHTML = "Não é um número válido";
+    document.getElementById('baixo').innerHTML = "Não é um número válido";
+    document.getElementById('decimais').innerHTML = "Não é um número válido";
+} else {
+
+    document.getElementById('nan').innerHTML = "Não";
+
+    if (numero >= 0) {
+        document.getElementById('raiz').innerHTML = Math.sqrt(numero);
+    } else {
+        document.getElementById('raiz').innerHTML = "Não existe raiz real de número negativo";
+    }
+
+    const ehInteiro = numero % 1 === 0;
+    document.getElementById('inteiro').innerHTML = ehInteiro ? "Sim" : "Não";
+
+    document.getElementById('cima').innerHTML = Math.ceil(numero);
+
+    document.getElementById('baixo').innerHTML = Math.floor(numero);
+
+    document.getElementById('decimais').innerHTML = numero.toFixed(2);
+}
